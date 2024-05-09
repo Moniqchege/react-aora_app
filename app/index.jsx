6177c2f8db37e25c0a0b18db9ee6e67e4e6ee3d6
Moniqchege <quichege33@gmail.com>
2024-05-09 09:39:25 +0300

    commit

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,16 @@
+import { View, Text } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
 import { Link } from 'expo-router'
+import React from 'react'
 
-export default function App() {
+const index = () => {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text>Aora!</Text>
+      <Text className="text-3xl font-pblack">Aora!</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" style={{ color: 'blue'}}>Go to Profile</Link>
+      <Link href="/home" style={{ color: 'blue' }}>Go to Home</Link>
     </View>
   )
 }
 
+export default index
